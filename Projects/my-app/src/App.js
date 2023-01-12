e1c760b8f1bbdd98f928436b093cbd968ac60595
Jephter Maurice <mjephter2@gmail.com>
2023-01-11 18:01:52 -0800

    lesson: Lifting State in React

--- Projects/my-app/src/App.js
+++ Projects/my-app/src/App.js
@@ -1,5 +1,48 @@
 import * as React from "react";
 
+function App() {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <Search onSearch={handleSearch}/>
+
+      <hr />
+
+      <List list={searchedStories}/>
+    </div>
+  );
+}
+
 function List(props) {
   return (
     <ul>
@@ -22,58 +65,10 @@ const Item = (props) => (
 );
 
 const Search = (props) => {
-  const [searchTerm, setSearchTerm] = React.useState("");
-
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
-    props.onSearch(event);
-  };
-
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input type="text" id="search" onChange={handleChange} />
-
-      <p>
-        Searching for <strong>{searchTerm}</strong>.
-      </p>
-    </div>
-  );
-}
-
-function App() {
-  const stories = [
-    {
-      title: "React",
-      url: "https://reactjs.org",
-      author: "Jordan Walke",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: "Redux",
-      url: "https://redux.js.org",
-      author: "Dan Abramov, Andrew Clark",
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
-
-  const handleSearch = (event) => {
-    console.log(event);
-  };
-
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
-
-      <Search onSearch={handleSearch}/>
-
-      <hr />
-
-      <List list={stories}/>
+      <input type="text" id="search" onChange={props.onSearch} />
     </div>
   );
 }
