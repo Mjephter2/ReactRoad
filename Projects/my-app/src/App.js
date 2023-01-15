4136ca2083e2518bfd4e0913243b7b82e43fc624
Jephter Maurice <mjephter2@gmail.com>
2023-01-15 12:08:21 -0800

    lesson: Imperative React

--- Projects/my-app/src/App.js
+++ Projects/my-app/src/App.js
@@ -57,6 +57,7 @@ function App() {
         id="search"
         value={searchTerm}
         onInputChange={handleSearch}
+        isFocused
       > 
         <strong>Search:</strong>
       </InputWithLabel>
@@ -89,15 +90,35 @@ const Item = ({ title, url, author, num_comments, points }) => (
   </li>
 );
 
-const InputWithLabel = ({ id, value, type="text", onInputChange, children}) => (
-  <>
-    <label htmlFor={id}>{children}</label>
-    <input 
-    value={value} 
-    type={type} 
-    id={id} 
-    onChange={onInputChange} />
-  </>
-);
+const InputWithLabel = ({
+  id,
+  value,
+  type = "text",
+  onInputChange,
+  children,
+  isFocused,
+}) => {
+  const inputRef = React.useRef();
+
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+
+  return (
+    <>
+      <label htmlFor={id}>{children}</label>
+      <input
+        ref={inputRef}
+        value={value}
+        type={type}
+        id={id}
+        autoFocus={isFocused}
+        onChange={onInputChange}
+      />
+    </>
+  );
+};
 
 export default App;
