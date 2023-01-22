66fcfe02323904254f1389410eee9088b9ba0d0b
Jephter Maurice <mjephter2@gmail.com>
2023-01-21 17:21:42 -0800

    lesson: Forms in React

--- Projects/my-app/src/App.js
+++ Projects/my-app/src/App.js
@@ -1,6 +1,27 @@
 import * as React from "react";
 import axios from 'axios';
 
+const SearchForm = ({
+  searchTerm, 
+  onSearchInput, 
+  onSearchSubmit,
+}) => (
+  <form onSubmit={onSearchSubmit}>
+    <InputWithLabel
+      id="search"
+      value={searchTerm}
+      onInputChange={onSearchInput}
+      isFocused
+    >
+      <strong>Search:</strong>
+    </InputWithLabel>
+
+    <button type="submit" disabled={!searchTerm}>
+      Submit
+    </button>
+  </form>
+)
+
 const storiesReducer = (state, action) => {
   switch (action.type) {
     case "STORIES_FETCH_INIT":
@@ -70,6 +91,7 @@ function App() {
 
   const handleSearchSubmit = (event) => {
     setUrl(`${API_ENDPOINT}${searchTerm}`);
+    event.preventDefault();
   };
 
   const handleFetchStories = React.useCallback(async () => {
@@ -101,22 +123,11 @@ function App() {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <InputWithLabel
-        id="search"
-        value={searchTerm}
-        onInputChange={handleSearchInput}
-        isFocused
-      >
-        <strong>Search:</strong>
-      </InputWithLabel>
-
-      <button
-        type="button"
-        disabled={!searchTerm}
-        onClick={handleSearchSubmit}
-      >
-        Submit
-      </button>
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
 
       <hr />
 
